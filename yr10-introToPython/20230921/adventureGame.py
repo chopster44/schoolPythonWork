f7eac4a883ba1688f0b48c98bdae7000903e343b
chopster44 <oscar@chopster44.com>
2023-09-22 21:39:13 +0100

    [Change] Rename generated_room

--- yr10-introToPython/20230921/adventureGame.py
+++ yr10-introToPython/20230921/adventureGame.py
@@ -6,21 +6,21 @@ player: str = "@"
 
 def room_generator(size: int) -> list[list[str]]:
     size = size * 2 + 1
-    generated_map: list[list[str]] = [[]]
+    generated_room: list[list[str]] = [[]]
     for i in range(0, size):
         if i < (size - 1):
-            generated_map.append([])
+            generated_room.append([])
         for j in range(0, size):
-            generated_map[i].append(" ")
+            generated_room[i].append(" ")
     for i in range(0, size):
         for j in range(0, size):
             if i == 0 or i == (size - 1):
-                generated_map[i][j] = "-"
+                generated_room[i][j] = "-"
             elif j == 0 or j == (size - 1):
-                generated_map[i][j] = "|"
+                generated_room[i][j] = "|"
             else:
-                generated_map[i][j] = " "
-    return generated_map
+                generated_room[i][j] = " "
+    return generated_room
 
 
 def draw_player(pos: list[int], icon: str):
