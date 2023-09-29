d5c3e47d23c09ffbea9165e682335122b7555231
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-09-29 08:50:24 +0000

    [ADDED] First Task

---
+++ yr10-introToPython/20230929/whileLoops1.py
@@ -0,0 +1,9 @@
+while True:
+    puppies: str=str(input("Do you like puppies")).lower()
+    if puppies == "no":
+        print("Wrong answer")
+    elif puppies == "yes":
+        print("Right answer")
+        break
+    else:
+        print("Error")
