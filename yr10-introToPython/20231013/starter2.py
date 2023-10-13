cd312acca4e06b9c785f56e28afc4240d5186329
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-13 08:17:39 +0000

    [ADDED] Starter 2

---
+++ yr10-introToPython/20231013/starter2.py
@@ -0,0 +1,8 @@
+# Ask the user how many times the dog should bark.
+num_barks = int(input("How many times should the dog bark? "))
+
+# Use a for loop to make the dog bark the specified number of times.
+for i in range(num_barks):
+  print("Woof!")
+
+print("Dog is done barking.")
