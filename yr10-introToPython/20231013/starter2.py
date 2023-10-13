23b0f776ac014c4e7dedf34590df9fd9be6671db
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-13 08:18:52 +0000

    [FIXED] Indentation on starter 2

--- yr10-introToPython/20231013/starter2.py
+++ yr10-introToPython/20231013/starter2.py
@@ -3,6 +3,6 @@ num_barks = int(input("How many times should the dog bark? "))
 
 # Use a for loop to make the dog bark the specified number of times.
 for i in range(num_barks):
-  print("Woof!")
+    print("Woof!")
 
 print("Dog is done barking.")
