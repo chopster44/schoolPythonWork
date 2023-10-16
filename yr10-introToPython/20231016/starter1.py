36b0a2b44c132414cb3df802efa652df96b4b43e
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-16 13:21:22 +0000

    [ADDED] Atempt counter

--- yr10-introToPython/20231016/starter1.py
+++ yr10-introToPython/20231016/starter1.py
@@ -2,16 +2,19 @@ from random import randint
 # Generate a random number between 1 and 10
 secret_number: int = randint(1, 10)
 
+attempt_taken: int = 0
+
 # Allow the user to guess the number
 for attempts in range(10):
     guess: int = int(input("Guess the number (1-10): "))
+    attempt_taken += 1
 
     if guess == secret_number:
-        print("Congratulations! You guessed the secret number.")
+        print(f"Congratulations! You guessed the secret number. You took {attempt_taken} attempts")
         break
     else:
         if guess > secret_number:
-                print("Try a higher number.")
+            print("Try a higher number.")
         elif guess < secret_number:
             print("Try a lower number.")
         else: 
