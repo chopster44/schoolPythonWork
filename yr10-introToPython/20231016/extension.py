c21b0101baf5385b813307453cfb5d9ace887a5f
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-16 13:57:30 +0000

    [ADDED] Main task

---
+++ yr10-introToPython/20231016/extension.py
@@ -0,0 +1,22 @@
+import keyboard
+
+# text colours
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+print(f"{}")
+# main loop
+while True:
+    # show menu:
+
+    pass
