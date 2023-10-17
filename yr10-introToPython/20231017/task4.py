71d0575d196d0a8c194d15592225675f0202e048
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-17 13:02:00 +0100

    [ADDED] task4

---
+++ yr10-introToPython/20231017/task4.py
@@ -0,0 +1,12 @@
+import math
+from random import randint
+rolls: list[int] = []
+while True:
+  action: str = input("Roll (r) or Quit (Q)")
+  if action == "r":
+    tempRand: int = randint(1,6)
+    rolls.append(tempRand)
+    print(tempRand)
+  else:
+    print(f"Average roll {math.floor(sum(rolls)/len(rolls))}")
+    break
