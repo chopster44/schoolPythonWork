8b7a573f0dfc5e249ec8dbc1ae24e31831f89c9a
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-10-19 10:49:04 +0000

    [ADDED] If statements 1

---
+++ yr10-introToPython/20231019/ifStatements1.py
@@ -0,0 +1,10 @@
+english: int = int(input("What number do you want to say (numeral)? "))
+
+if english == 1:
+    print("Uno")
+elif english == 2:
+    print("Dos")
+elif english == 3:
+    print("Tres")
+else:
+    print("I don't know numbers over 3")
