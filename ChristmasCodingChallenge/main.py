80281b0e967793c159e57211b9bbf8a1f5c59337
chopster44 <62182317+chopster44@users.noreply.github.com>
2023-12-18 14:57:15 +0000

    Did the christams coding challenge

--- ChristmasCodingChallenge/main.py
+++ ChristmasCodingChallenge/main.py
@@ -1,2 +1,10 @@
-# Challenge 8
-# Advent calendar
+def get_advent_calendar_content() -> list[str]:
+    file = open("adventCalendar.txt", "r", encoding="UTF-8")
+    return file.read().split("\n")
+
+def main():
+    day: int = int(input("What day is it? "))
+    adventCalendarContent: list[str] = get_advent_calendar_content()
+    print(adventCalendarContent[day])
+
+main()
