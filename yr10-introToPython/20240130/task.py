97a663b254b25224ae1f54cd554e047cd2dbf63f
chopster44 <62182317+chopster44@users.noreply.github.com>
2024-01-30 13:17:11 +0000

    completed task

---
+++ yr10-introToPython/20240130/task.py
@@ -0,0 +1,24 @@
+# name = input("What is your name: ")
+# print(f"{name.upper()}")
+
+# password = "oatmeal"
+# if input("What is you password?" ).lower() == password:
+# 	print("Correct Password!")
+# else:
+#     print("Access denied")
+
+# string = input("What is your first string?") + input("What is your second string?")
+# print(len(string))
+
+# goat = input("$ ")
+# print(f"{goat[1]}")
+
+# camel = input("$ ")
+# print(camel[2:])
+
+# cheese = input("$ ")
+# print(cheese[:-2])
+
+string = input("What is your string?")
+char = input("What string do you want to count? ")
+print( string.count(char))
