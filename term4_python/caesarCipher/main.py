aa6932ab4b2776a33d70b47e196eba29de7033e5
chopster44 <62182317+chopster44@users.noreply.github.com>
2024-03-04 15:09:38 +0000

    Finished caeser cipher

--- term4_python/caesarCipher/main.py
+++ term4_python/caesarCipher/main.py
@@ -5,13 +5,11 @@ def encrypt (original: str, shift: int) -> str:
     for i in original:
         oldIndex: int = asciiTable.index(i)
         newIndex: int = oldIndex + shift
-        if i.isupper():
-            if newIndex > 90:
-                newIndex -= 26
-        elif i.islower():
-            if newIndex > 122:
-                newIndex -= 26
-        print(oldIndex, newIndex, asciiTable[newIndex])
+        newIndex -= 26
+        if newIndex < 0:
+             newIndex += 128
+        elif newIndex > 128:
+             newIndex -= 128
         rtrnString += asciiTable[newIndex]
     return rtrnString
 
@@ -20,15 +18,26 @@ def decrypt (encrypted: str, shift: int) -> str:
     for i in encrypted:
         oldIndex: int = asciiTable.index(i)
         newIndex: int = oldIndex - shift
-        if i.isupper():
-            if newIndex < 65:
-                newIndex += 26
-        elif i.islower():
-            if newIndex > 97:
-                newIndex += 26
-        print(oldIndex, newIndex, asciiTable[newIndex])
+        if newIndex < 0:
+             newIndex += 128
+        elif newIndex > 128:
+             newIndex -= 128               
         rtrnString += asciiTable[newIndex]
     return rtrnString
+
+def bruteforce (encrypted: str, commonLetter: str) -> str:
+    mostLetter: int = 0
+    largestN: int = 0
+    for i in asciiTable:
+        if encrypted.count(i) > largestN:
+            mostLetter = asciiTable.index(i)
+    for i in range(mostLetter, len(asciiTable)):
+        if asciiTable[i] == commonLetter:
+            shift = mostLetter - i
+    for i in range(0, mostLetter):
+         if asciiTable[i] == commonLetter:
+            shift = mostLetter + i
+    return decrypt(encrypted, shift)
 
 func: str = input("Encrypt or Decrypt [e/d]? \n $ ")
 if func.lower() == "e":
@@ -39,3 +48,7 @@ elif func.lower() == "d":
     inptString: str = input("What do you want to decrypt? \n $ ")
     inptShift: int = int(input("How much is it shifted by? \n $ "))
     print(f"The decrypted string is\n {decrypt(inptString, inptShift)}")
+elif func.lower() == "b":
+    print(" --- Brute force mode --- ")
+    inptString: str = input("What would you like to decode? \n $")
+    print(f"Decoded string is: {bruteforce(inptString, 'e')}")
