30358cc381b3dcd0425e8a2aac29c04ce7f365e7
chopster44 <62182317+chopster44@users.noreply.github.com>
2024-04-26 09:00:04 +0000

    started sorting algorithms

---
+++ sortingAlgorithms/bubbleSort/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void checkValues(int *array, int index) {
+    if (array[index] < array[index + 1]) {
+        
+    }
+}
+
+int main() {
+    int array[7] = {3, 9, 2, 10, 8, 5, 1};
+
+}
