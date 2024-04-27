d7fd0fb9fd4c681d9fb47d0ebd63c7aadc5880e5
chopster44 <oscar@chopster44.com>
2024-04-27 13:59:06 +0100

    Finished bubble sort

--- sortingAlgorithms/bubbleSort/main.c
+++ sortingAlgorithms/bubbleSort/main.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 
-void checkValues(int *array, int index) {
-    if (array[index] < array[index + 1]) {
-        
+#define LEN(x)  (sizeof(x) / sizeof((x)[0]))
+
+void printArray(int *array, int arraySize) {
+    printf(" {");
+    for (int i = 0; i < arraySize; i++) {
+        printf("%i", array[i]);
+        if (i < (arraySize - 1)) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+}
+
+int checkValues(int *array, int index, int len) {
+    if (array[index] > array[index + 1]) {
+        int t = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = t;
+        if (index < (len - 1)) {
+            checkValues(array, index + 1, len);
+        }
+        return 1;
+    } else {
+        if (index == (len - 1))
+            return 0;
+        return checkValues(array, index + 1, len);
     }
 }
 
-int main() {
-    int array[7] = {3, 9, 2, 10, 8, 5, 1};
+void bubble(int *array, int len) {
+    if (checkValues(array, 0, len-1))
+        bubble(array, len);
+    return;
+}
 
+int main() {
+    printf("Unsorted:");
+    int array[7] = {3, 9, 2, 10, 8, 5, 1};
+    int i;
+    printArray(array, LEN(array));
+    int sorted = 0;
+    i = 0;
+    bubble(array, LEN(array));
+
+    printf("Sorted:");
+    printArray(array, LEN(array));
 }
